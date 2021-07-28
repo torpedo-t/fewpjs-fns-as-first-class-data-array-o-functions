cf62f0b46edf3b84920d40a140d7e468c1a0f29f
torpedo-t <55634516+torpedo-t@users.noreply.github.com>
2021-07-28 17:12:22 -0500

    Done.

---
+++ index.js
@@ -0,0 +1,26 @@
+function wakeDog(dogName, dogBreed) {
+    const a = dogName
+    const b = dogBreed
+    return 'Wake ${a} the ${b}'
+    // console.log('Wake ${dogName} the ${dogBreed}')
+}
+
+function leashDog(dogName, dogBreed) {
+    console.log('Leash ${dogName} the ${dogBreed}')
+}
+
+function walkToPark(dogName, dogBreed) {
+    console.log('Walk to the park with ${dogName} the ${dogBreed}')
+}
+
+function throwFrisbee(dogName, dogBreed) {
+    console.log('Throw the frisbee for ${dogName} the ${dogBreed}')
+}
+
+function walkHome(dogName, dogBreed) {
+    console.log('Walk home with ${dogName} the ${dogBreed')
+}
+
+function unleashDog(dogName, dogBreed) {
+    console.log('Unleash ${dogName} the ${dogBreed}')
+}
