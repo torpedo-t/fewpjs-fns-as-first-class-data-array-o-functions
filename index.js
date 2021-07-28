30598c06315a7673a87e1a86dfa055da6313d864
torpedo-t <55634516+torpedo-t@users.noreply.github.com>
2021-07-28 17:55:06 -0500

    Done.

--- index.js
+++ index.js
@@ -1,26 +1,33 @@
 function wakeDog(dogName, dogBreed) {
-    const a = dogName
-    const b = dogBreed
-    return 'Wake ${a} the ${b}'
+    return `Wake ${dogName} the ${dogBreed}`
     // console.log('Wake ${dogName} the ${dogBreed}')
 }
 
 function leashDog(dogName, dogBreed) {
-    console.log('Leash ${dogName} the ${dogBreed}')
+    return `Leash ${dogName} the ${dogBreed}`
 }
 
 function walkToPark(dogName, dogBreed) {
-    console.log('Walk to the park with ${dogName} the ${dogBreed}')
+    return `Walk to the park with ${dogName} the ${dogBreed}`
 }
 
 function throwFrisbee(dogName, dogBreed) {
-    console.log('Throw the frisbee for ${dogName} the ${dogBreed}')
+    return `Throw the frisbee for ${dogName} the ${dogBreed}`
 }
 
 function walkHome(dogName, dogBreed) {
-    console.log('Walk home with ${dogName} the ${dogBreed')
+    return `Walk home with ${dogName} the ${dogBreed}`
 }
 
 function unleashDog(dogName, dogBreed) {
-    console.log('Unleash ${dogName} the ${dogBreed}')
+    return `Unleash ${dogName} the ${dogBreed}`
+}
+
+let routine = [wakeDog, leashDog, walkToPark, throwFrisbee, walkHome, unleashDog]
+
+function exerciseDog(dogName, dogBreed) {
+    let array = routine
+    for(let i = 0; i < array.length; i++) {
+        array.push()
+    }
 }
