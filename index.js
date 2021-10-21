c3e4a30bc1824cf49eead6f2b7b1c677c385bee4
torpedo-t <55634516+torpedo-t@users.noreply.github.com>
2021-10-21 14:16:52 -0500

    commit tree 37e0802eec970b46a584b95ddb5123f82ce98fa9

--- index.js
+++ index.js
@@ -1,28 +1,28 @@
 function wakeDog(dogName, dogBreed) {
     return `Wake ${dogName} the ${dogBreed}`
-}
+};
 
 function leashDog(dogName, dogBreed) {
     return `Leash ${dogName} the ${dogBreed}`
-}
+};
 
 function walkToPark(dogName, dogBreed) {
     return `Walk to the park with ${dogName} the ${dogBreed}`
-}
+};
 
 function throwFrisbee(dogName, dogBreed) {
     return `Throw the frisbee for ${dogName} the ${dogBreed}`
-}
+};
 
 function walkHome(dogName, dogBreed) {
     return `Walk home with ${dogName} the ${dogBreed}`
-}
+};
 
 function unleashDog(dogName, dogBreed) {
     return `Unleash ${dogName} the ${dogBreed}`
-}
+};
 
-const routine = [wakeDog, leashDog, walkToPark, throwFrisbee, walkHome, unleashDog]
+const routine = [wakeDog, leashDog, walkToPark, throwFrisbee, walkHome, unleashDog];
 
 // iterate over routine array to get access to each function
 // call each function in the array, passing dogName, dogBreed received by exerciseDog function
@@ -35,5 +35,5 @@ function exerciseDog(dogName, dogBreed) {
         let exercise = fn(dogName, dogBreed)
         newArray.push(exercise)
     }
-    return newArray
+    return newArray;
 }
